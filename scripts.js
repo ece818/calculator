ef64cc7c0acb9c65b33e1d5967287a25fd945d23
Erik Elliott <erikcelliott@gmail.com>
2025-10-25 03:32:10 -0400

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   .vscode/launch.json
    	modified:   scripts.js
    	modified:   style.css

--- scripts.js
+++ scripts.js
@@ -1,24 +1,49 @@
 const display = document.getElementById("display");
-
+let lastChar = "0";
 function appendToDisplay(input) {
-  if (display.value === 0) {
+  getLastChar();
+  if (display.value === "0") {
     if (input == "+" || input == "*" || input == "/" || input == "0") {
       display.value = "Enter A Number";
       setTimeout(clearDisplay, 1500);
     } else if (input == ".") {
       display.value = "0.";
+      getLastChar();
     } else {
       display.value = input;
+      getLastChar();
     }
+  } else if (
+    (lastChar === "+" ||
+      lastChar === "*" ||
+      lastChar === "/" ||
+      lastChar === "." ||
+      lastChar === "-") &&
+    (input === "+" ||
+      input === "*" ||
+      input === "/" ||
+      input === "." ||
+      input === "-")
+  ) {
+    let beforeError = display.value;
+    display.value = `You entered ${input} which is the same as ${lastChar}`;
+    setTimeout(() => {
+      display.value = beforeError;
+      getLastChar();
+    }, 1500);
   } else {
     if (display.value === "0.") {
       display.value = `0.${input}`;
+      getLastChar();
     } else {
       display.value += input;
+      getLastChar();
     }
   }
 }
-
+function getLastChar() {
+  lastChar = display.value.at(-1);
+}
 function clearDisplay() {
   display.value = "0";
 }
